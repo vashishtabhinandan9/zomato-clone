d31472a0201e9c4f5281909df600f9ae2a8b620e
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2022-05-03 00:28:31 +0530

    day 68,69 add restaurant and image reducer

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+
+import Store from "./Redux/store";
 
 
 // Import css files
@@ -13,10 +16,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
+
   <React.StrictMode>
+    <Provider store={Store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-    
+    </Provider>
   </React.StrictMode>
 );
